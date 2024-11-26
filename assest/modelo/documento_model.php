bf78c729dc0bc2b95f22f08d48bfbb220b1b351d
Michael Alexis Salas Gonzalez <michael@mscode.cl>
2024-11-26 01:18:26 -0300

    Update documento_model.php

--- assest/modelo/documento_model.php
+++ assest/modelo/documento_model.php
@@ -10,6 +10,7 @@ class DocumentoModel {
 
     public function getDocumentosByProductor($productorId) {
         $query = "SELECT * FROM tb_documento WHERE productor_documento = :productorId AND estado_documento = 1";
+        //echo $productorId.' '.$query;
         $stmt = $this->db->prepare($query);
         $stmt->execute(['productorId' => $productorId]);
         return $stmt->fetchAll(PDO::FETCH_OBJ);
